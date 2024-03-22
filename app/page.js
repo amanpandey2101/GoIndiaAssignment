9be64e8336dd62c2c5996ec62236a54960e3ab12
amanpandey2101 <paman7681@gmail.com>
2024-03-22 11:37:37 +0530

    title change

--- app/page.js
+++ app/page.js
@@ -2,12 +2,8 @@ import Layout from "./components/Layout";
 
 export default function Home() {
   return (
-   
-       <Layout>
-      <div>
-      
-      </div>
+    <Layout>
+      <div></div>
     </Layout>
-  
   );
 }
